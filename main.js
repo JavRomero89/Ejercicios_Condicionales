d52f399d0a768d36fae328223d4ad37f58f77825
JavRomero89 <javieromero23@gmail.com>
2023-06-28 18:49:27 +0200

    Advance

---
+++ main.js
@@ -0,0 +1,23 @@
+// Ejercicio 1
+number=prompt("Inserte un dato un numero del 0 al 10")
+
+if(number>=5){
+    console.log(`El número introducido ${number} es mayor a 5`);
+}
+else{
+    console.log(`El número introducido ${number} no es mayor a 5`);
+}
+
+
+// Ejercicio 2
+ if (number == 5){
+    console.log(`El numero introducido ${number} es igual a 5`);
+}
+ else if(number<=5){
+    console.log(`El numero introducido ${number} es menor a 5`);
+
+}
+else{
+    console.log(`El numero introducido ${number} es mayor a 5`);
+}
+
